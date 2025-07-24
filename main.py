facec51aa4e0c79223a4d7aeb2e15967966998e1
BDTCastle <punktowntv@gmail.com>
2025-07-24 04:20:39 -0700

    Implement basic game loop and window

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # 1. Handle user input (events)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # 2. Draw the game world
+        screen.fill("black")
+
+        # 3. Update the display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
